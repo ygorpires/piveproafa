2f7c0427b039d296adf4b91cb9a86561e35cbcff
ygorpires <ygor_pires@hotmail.com>
2019-04-22 01:45:56 -0300

    Comit Inicial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn veproafa.wsgi --log-file -
---
+++ veproafa/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from home import urls as home_urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(home_urls)),
+    path('login/',auth_views.LoginView.as_view(), name='login'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
